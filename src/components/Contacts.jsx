f25e7827e4aba191e38493b24ff0d9cbdcd8324c
jkunciene <j.kunciene@gmail.com>
2022-06-08 11:49:11 +0300

    first commit

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Contacts = (props) => {
+
+    console.log(props);
+
+  return (
+    <div>
+        <p>cia yra mano kontaktu komponentas</p>
+        <h2>vardas: {props.name}</h2>
+        <h3>email: {props.email}</h3>
+        <h3>age: {props.age} </h3>
+        <p>cia yra mano kortele</p>
+
+    </div>
+  )
+}
+
+export default Contacts
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Contacts from "./Contacts";
+
+const Main = () => {
+  return (
+    <div>
+
+        <Contacts 
+            name={"Jolita"}
+            email={"jolita@gmail.com"}
+            age={18}/>
+    </div>
+  )
+}
+
+export default Main
