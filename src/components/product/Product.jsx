215f2981509c1b6e8326006ad7c47c332d36fa89
jkunciene <j.kunciene@gmail.com>
2022-06-09 13:33:51 +0300

    iki kiekio didinimo

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import data from '../../data/data';
+import './Product.css';
 
-const Product = () => {
+const Product = ({addToCart}) => {
     console.log(data);
   return (
     <div className='itemList'>
         {
             data.map((product, index)=> (
-                <div className='item'>
+                <div key={index} className='item'>
                     <img src={product.imageUrl} alt="" />
-                    <p key={index}>Product: {product.name}</p>
+                    <p >Product: {product.name}</p>
                     <p>Description: {product.description}</p>
                     <p>Price: {product.price} $</p>
-                    <button>Buy</button>
+                    <button onClick={()=>addToCart(product)}>Buy</button>
 
                 </div>
             ))
--- src/components/productslist/ProductsList.jsx
+++ src/components/productslist/ProductsList.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from '../product/Product'
 
 const ProductsList = () => {
+
+    //state naudosiu prekiu krepseliui
+    const [items, setItems] = useState([]);
+
+    //pridejimo i krepseli funkcija
+    const addToCart = (product) => {
+        console.log(product);
+
+        //1-tikrinti, ar tokia preke jau yra krepselyje
+        //2-jei nera prideti savybe kiekis ir reiksme 1
+        //3-jei preke yra surasti savybe kiekis ir ja padidinti ++
+
+    }
   return (
     <div>
-        <Product/>
+        <Product addToCart={addToCart}/>
     </div>
   )
 }
